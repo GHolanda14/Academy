63104c5e80d296eef7198f2e26b901918f6fb8e1
gholanda14 <gabriel.raulino14@gmail.com>
2021-01-08 19:41:40 -0300

    only a few changes

--- Practice/Javascript/script.js
+++ Practice/Javascript/script.js
@@ -1,19 +1,30 @@
-/*  Here I'm removing an element from HTML page using his ID. Also adding another element, without deleting or overwriting the paragraph existent.*/
+/* Here I'm adding another phrase for my p tag, also disabling my button to avoid to repeat the phrase.
+*/
 function teste(){
     document.getElementById('paragrafo').innerHTML += '<br>Além de tudo, precisamos testar mais coisas com essas atividades perpedinculares nas vidas atômicas.'
-    document.getElementById('botao').remove();
-
-    let container = document.getElementById('container');
-    container.innerHTML += '<button type="button" id="botao" disabled>Already done</button';
+    document.getElementById('botao').disabled = true;
 }
 
 /*This is an example of forLoop, just putting some buttons with numbers on it.
-It's funny because I've tried to use ${} to concat the string, but it didn't work*/
+Furthermore, avoiding that the user repeat the action.*/
 function forLoop(){
     let div = document.querySelector('.caixinhas');
     let html = '';
+    let botao = document.querySelector('[onclick = "forLoop()"');
+    botao.disabled = true;
+    botao.innerHTML = 'Already done';
+    
     for(let cont = 1; cont <= 10;cont++){
-        html += '<button>'+cont+'</button>';
-    }
+        html += `<button>${cont}</button>`;
+    } 
     div.innerHTML += html;
 }
+
+/*Begining with arrowfunctions and using default parameters*/
+
+function calculate(numeros){
+    let valores = document.getElementsByName(numeros);
+    soma(parseInt(valores[0].value),parseInt(valores[1].value));
+}
+
+const soma = (n1 = 0,n2 = 0) => console.log(n1+n2);
